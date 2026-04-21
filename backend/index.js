229e3b08464f67ee689eddbcfafa69c48ed65a4d
moath-alhussami <alhussami.moath@gmail.com>
2026-04-21 07:29:45 +0000

    backend cod update

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// ✅ Add CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the Backend! 🚀' });
 });
